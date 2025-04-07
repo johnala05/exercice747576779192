3dea031c1a800e0ed559446daa31563a30f53fd6
johnala05 <johnalarousseau05@gmail.com>
2025-04-07 17:33:26 -0400

    Add files via upload

---
+++ Exercice 74757677.py
@@ -0,0 +1,15 @@
+#7.4
+things = ["mozzarella", "cinderella", "salmonella"]
+print(things)
+
+#7.5
+things[1] = things[1].capitalize()
+print(things)
+
+#7.6
+things[0] = things[0].upper()
+print(things)
+
+#7.7
+del things[2]
+print(things)
